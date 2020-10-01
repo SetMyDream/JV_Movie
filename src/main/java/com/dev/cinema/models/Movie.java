9e164826dac39b295fbf494c611f969cf5227468
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-10-01 13:46:47 +0300

    db connection established

--- src/main/java/com/dev/cinema/models/Movie.java
+++ src/main/java/com/dev/cinema/models/Movie.java
@@ -18,8 +18,30 @@ public class Movie {
     @Column(name = "movie_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
-
     private String title;
-
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
