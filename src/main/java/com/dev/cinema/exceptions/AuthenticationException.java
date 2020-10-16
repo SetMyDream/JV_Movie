aca2a93d3425d7b2acb03ae921f1d9aef90e867e
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-10-16 03:06:18 +0300

    added logger to Errors

--- src/main/java/com/dev/cinema/exceptions/AuthenticationException.java
+++ src/main/java/com/dev/cinema/exceptions/AuthenticationException.java
@@ -1,11 +1,18 @@
 package com.dev.cinema.exceptions;
 
+import org.apache.log4j.Logger;
+
 public class AuthenticationException extends Exception {
+
+    private static final Logger logger = Logger.getLogger(RuntimeException.class);
+
     public AuthenticationException(String message, Throwable cause) {
         super(message, cause);
+        logger.error(message,cause);
     }
 
     public AuthenticationException(String message) {
         super(message);
+        logger.error(message);
     }
 }
